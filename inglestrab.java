50ce398a1c133f5b878bb253d29921d760966272
Analina <dutranacrolina@gmail.com>
2026-03-20 17:06:02 -0300

    Creating the lists

---
+++ inglestrab.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class inglestrab {
+
+    // Kanban lists
+    static ArrayList<String> todo = new ArrayList<>();
+    static ArrayList<String> doing = new ArrayList<>();
+    static ArrayList<String> done = new ArrayList<>();
+    // Scanner to read user input
+    static Scanner sc = new Scanner(System.in);
+}
